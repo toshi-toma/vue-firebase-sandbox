a4d8b53f0b8f4117fc6a5582c3b8dbbc18de64f9
toshi-toma <toshitoma1217@gmail.com>
2020-02-29 12:24:04 +0900

    add firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,44 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from "firebase";
+// Required for side-effects
+import "firebase/firestore";
+
+const firebaseConfig = {
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+db.collection("users").add({
+  first: "Ada",
+  last: "Lovelace",
+  born: 1815
+})
+.then(function(docRef) {
+  console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+  console.error("Error adding document: ", error);
+});
+db.collection("users").doc("LA").set({
+  first: "Ada",
+  last: "Lovelace",
+  born: 1815
+})
+.then(function() {
+  console.log("Document successfully written!");
+})
+.catch(function(error) {
+  console.error("Error writing document: ", error);
+});
+
+db.collection("users").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+  });
+});
 
 Vue.config.productionTip = false
 
